5c50c41482f1097a2bab040776d484192a91af70
whitedukex <100641806+whitedukex@users.noreply.github.com>
2022-03-01 02:39:56 +0300

    Add files via upload

---
+++ df.py
@@ -0,0 +1,40 @@
+# ghoul
+# import our modules
+import time
+from colorama import Fore, Back
+from colorama import init
+init()
+# print phrase from anime
+# and add some color here
+print(Fore.WHITE)
+time.sleep(1.550)
+print("I'm...")
+time.sleep(1.800)
+# and here
+print(Fore.RED)
+print("Ghoul...")
+time.sleep(1.000)
+
+# create function
+
+def ghoul():
+	# variable from which we will calculate 7
+	a = 1000
+	# cycle
+	while a > 0:
+		a = a - 7
+		b = a + 7 
+		# formatting and print
+		# aaand here some color
+		time.sleep(0.050)
+		print(Fore.WHITE)
+		print(Back.RED)
+		print(f"{b}-7={a}") 
+# start out function
+ghoul()
+
+
+
+
+		
+
